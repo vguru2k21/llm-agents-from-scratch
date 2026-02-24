aff58a78de34eabc6063668c358c416fe3545aaf
vguru2k21 <76853934+vguru2k21@users.noreply.github.com>
2026-02-23 19:53:19 -0600

    Update llm.py

--- src/llm-agents-from-scratch/base/llm.py
+++ src/llm-agents-from-scratch/base/llm.py
@@ -1,1 +1,11 @@
+"""Base LLM."""
 
+from abc import ABC, abstractmethod
+from typing import Any, Sequence, TypeAlias, TypeVar
+from pydantic import BaseModel
+
+from llm_agents_from_scratch.data_structures import (
+  ChatMessage,
+  CompleteResult,
+  TooCallResult,
+)
